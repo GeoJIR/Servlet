ad92c6444bad19255cb5788aa43f4db4bec4223c
MiloIgor <igor.milovidoff@orange.fr>
2014-11-18 09:38:17 +0100

    Hard list users returned on doGet function

--- src/com/geojir/wbs/GeoJIR_wbsServlet.java
+++ src/com/geojir/wbs/GeoJIR_wbsServlet.java
@@ -1,15 +1,26 @@
 package com.geojir.wbs;
 import java.io.IOException;
-import javax.servlet.http.*;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 @SuppressWarnings("serial")
 public class GeoJIR_wbsServlet extends HttpServlet {
-	
+
+	  String[] listUsers = { "Martin,Joel", "Dupond,Marc", "Develt,Denis", "Fabre,Jean-Marc", "Duval,Pierre", "Garcia,José","Klein,Philippe","Reverso,Thierry" };
 	  @Override
 	  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException 
 	  {
-	      resp.setContentType("application/json");
-	      resp.getWriter().println("\"Hello, wellcome to GeoJIR's world (GET).\"");
+	      
+		  resp.setContentType("application/json");
+//	      resp.getWriter().println("\"Hello, welcome to GeoJIR's world (GET).\"");
+	      for(int i = 0; i< listUsers.length; i++)
+	      {
+	    	  resp.getWriter().println(listUsers[i]);
+	      }
 	  }
 
 	  @Override
@@ -18,10 +29,10 @@ public class GeoJIR_wbsServlet extends HttpServlet {
 
 	      if (req.getParameter("testing") != null) {
 	          resp.setContentType("application/json");
-	          resp.getWriter().println("\"Hello, wellcome to GeoJIR's world (POST).\"");
+	          resp.getWriter().println("\"Hello, welcome to GeoJIR's world (POST).\"");
 	      } else {
 		      resp.setContentType("application/json");
-		      resp.getWriter().println("\"Hello, wellcome to GeoJIR's world (POST).\"");
+		      resp.getWriter().println("\"Hello, welcome to GeoJIR's world (POST).\"");
 	      }
 	  }
 }
